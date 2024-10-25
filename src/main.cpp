49386a6f76cba3215c7425650274a52b747f06b4
Lucas Siqueira <lucas.asiq@gmail.com>
2024-10-25 17:02:57 +0100

    implement sub

--- src/main.cpp
+++ src/main.cpp
@@ -35,6 +35,17 @@ void get_memory_addresses(std::array<std::string, 8> &memory_addresses)
 
 std::array<std::string, 8> g_memory_addresses;
 
+void get_arithmetic_imm_reg_map(std::unordered_map<uint8_t, std::string> &arithmetic_imm_reg_map)
+{
+	arithmetic_imm_reg_map[0x00] = "ADD";
+	arithmetic_imm_reg_map[0x02] = "ADC";
+	arithmetic_imm_reg_map[0x05] = "SUB";
+	arithmetic_imm_reg_map[0x03] = "SBB";
+	arithmetic_imm_reg_map[0x07] = "CMP";
+
+}
+std::unordered_map<uint8_t, std::string> arithmetic_imm_reg_map;
+
 void disassemble_8086_opcode(const std::vector<uint8_t> &buffer, uint32_t &offset)
 {
 	uint8_t opcode = buffer[offset];
@@ -312,13 +323,14 @@ void disassemble_8086_opcode(const std::vector<uint8_t> &buffer, uint32_t &offse
 			offset+=3;
 		}
 	}
-	//ADD immediate to register/memory
+	//ADD/ADC/SUB/SBC/CMP immediate to register/memory
 	else if(opcode >> 2 == 0x20)
 	{
 		uint8_t w = opcode & 0x01;
 		uint8_t s = (opcode & 0x02) >> 1;
 		uint8_t modrm = buffer[offset + 1];
 		uint8_t mod = (modrm & 0xC0) >> 6;
+		uint8_t arith = (modrm & 0x38) >> 3;
 		uint8_t dst = modrm & 0x07;
 		if(mod == 0x00)
 		{
@@ -331,11 +343,11 @@ void disassemble_8086_opcode(const std::vector<uint8_t> &buffer, uint32_t &offse
 			}
 			if(s == 1)
 			{
-				std::cout << "ADD [" << memory_address << "], " << static_cast<int16_t>(imm) << std::endl;
+				std::cout << arithmetic_imm_reg_map[arith] << " [" << memory_address << "], " << static_cast<int16_t>(imm) << std::endl;
 			}
 			else 
 			{
-				std::cout << "ADD [" << memory_address << "], " << imm << std::endl;
+				std::cout << arithmetic_imm_reg_map[arith] <<  " [" << memory_address << "], " << imm << std::endl;
 			}
 			offset+=2;
 
@@ -353,11 +365,11 @@ void disassemble_8086_opcode(const std::vector<uint8_t> &buffer, uint32_t &offse
 			
 			if(s == 1)
 			{
-				std::cout << "ADD [" << memory_address << "+" << disp << "], " << static_cast<int16_t>(imm) << std::endl;
+				std::cout << arithmetic_imm_reg_map[arith] <<  " [" << memory_address << "+" << disp << "], " << static_cast<int16_t>(imm) << std::endl;
 			}
 			else
 			{
-				std::cout << "ADD [" << memory_address << "+" << disp << "], " << imm << std::endl;
+				std::cout << arithmetic_imm_reg_map[arith] <<  " [" << memory_address << "+" << disp << "], " << imm << std::endl;
 			}
 			offset+=3;
 		}
@@ -374,11 +386,11 @@ void disassemble_8086_opcode(const std::vector<uint8_t> &buffer, uint32_t &offse
 			}
 			if(s == 1)
 			{
-				std::cout << "ADD [" << memory_address << "+" << disp << "], " << static_cast<int16_t>(imm) << std::endl;
+				std::cout << arithmetic_imm_reg_map[arith] <<  " [" << memory_address << "+" << disp << "], " << static_cast<int16_t>(imm) << std::endl;
 			}
 			else
 			{
-				std::cout << "ADD [" << memory_address << "+" << disp << "], " << imm << std::endl;
+				std::cout << arithmetic_imm_reg_map[arith] <<  " [" << memory_address << "+" << disp << "], " << imm << std::endl;
 			}
 			offset+=4;
 		}
@@ -394,11 +406,11 @@ void disassemble_8086_opcode(const std::vector<uint8_t> &buffer, uint32_t &offse
 			}
 			if(s == 1)
 			{
-				std::cout << "ADD " << reg_dest << ", " << static_cast<int16_t>(imm) << std::endl;
+				std::cout << arithmetic_imm_reg_map[arith] << " " << reg_dest << ", " << static_cast<int16_t>(imm) << std::endl;
 			}
 			else
 			{
-				std::cout << "ADD " << reg_dest << ", " << imm << std::endl;
+				std::cout << arithmetic_imm_reg_map[arith] << " "  << reg_dest << ", " << imm << std::endl;
 			}
 			offset+=2;
 
@@ -418,7 +430,102 @@ void disassemble_8086_opcode(const std::vector<uint8_t> &buffer, uint32_t &offse
 		offset += 1 + w;
 	}
 
+	// SUB reg/memory with register to either
+	else if(opcode >> 2 == 0x0A)
+	{
+		uint8_t d = (opcode & 0x02) >> 1;
+		uint8_t w = opcode & 0x01;
+		uint8_t modrm = buffer[offset + 1];
+		uint8_t mod = (modrm & 0xC0) >> 6;
+		uint8_t src;
+		uint8_t dst;
+		
+		if (d == 0)
+		{
+			src = (modrm >> 3) & 0x07;
+			dst = modrm & 0x07;
+		}
+		else
+		{
+			src = modrm & 0x07;
+			dst = (modrm >> 3) & 0x07;
+		}
+		//Register to Register
+		if(mod == 0x03)
+		{
+			std::string reg_source = g_register_map[src][w];
+			std::string reg_dest = g_register_map[dst][w];
+			std::cout << "SUB " << reg_dest << ", " << reg_source << std::endl;
+			offset++;
 
+		}
+		else if (mod == 0x00)
+		{
+			std::string memory_address;
+			//immediate memory address to register
+			if ((d == 0 && dst == 0x06) || (d == 1 && src == 0x06)) 
+			{
+				uint16_t imm = buffer[offset + 2] | buffer[offset + 3] << 8;
+				memory_address = std::to_string(imm);
+				offset += 2;
+			}
+			else 
+			{
+				memory_address = d == 0 ? g_memory_addresses[dst] : g_memory_addresses[src];
+			}
+			
+			std::string reg = d == 0 ? g_register_map[src][w] : g_register_map[dst][w];
+			if(d == 0)
+			{
+				std::cout << "SUB [" << memory_address << "], " << reg << std::endl;
+			} else
+			{
+				std::cout << "SUB " << reg << ", [" << memory_address << "]" << std::endl;
+			}
+			offset++;
+		}
+		else if (mod == 0x01)
+		{	
+			std::string memory_address = d==0? g_memory_addresses[dst] : g_memory_addresses[src];
+			std::string reg = d == 0 ? g_register_map[src][w] : g_register_map[dst][w];
+			uint8_t disp = buffer[offset + 2];
+			if(d == 0)
+			{
+				std::cout << "SUB [" << memory_address << "+" << +disp << "], " << reg << std::endl;
+			} else
+			{
+				std::cout << "SUB " << reg << ", [" << memory_address << "+" << +disp << "]" << std::endl;
+			}
+			offset+=2;
+		}
+		else if(mod == 0x02)
+		{
+			std::string memory_address = d==0? g_memory_addresses[dst] : g_memory_addresses[src];
+			std::string reg = d == 0 ? g_register_map[src][w] : g_register_map[dst][w];
+			uint16_t disp = buffer[offset + 2] | buffer[offset + 3] << 8;
+			if(d == 0)
+			{
+				std::cout << "SUB [" << memory_address << "+" << +disp << "], " << reg << std::endl;
+			} else
+			{
+				std::cout << "SUB " << reg << ", [" << memory_address << "+" << +disp << "]" << std::endl;
+			}
+			offset+=3;
+		}
+	}
+	//SUB immediate to accumulator
+	else if(opcode >> 1 == 0x16)
+	{
+		uint8_t w = opcode & 0x01;
+		uint16_t imm = buffer[offset + 1];
+		if (w == 1)
+		{
+			imm |= buffer[offset + 2] << 8;
+		}
+		std::string reg = g_register_map[0x00][w];
+		std::cout << "SUB " << reg << ", " << imm << std::endl;
+		offset+=1+w;
+	}
 	else
 	{
 		std::cout << "Opcode not implemented: b0=0x" << std::hex << +opcode << std::endl;
@@ -455,6 +562,7 @@ int main(int argc, char* argv[])
 	read_file(filename, buffer);
 	get_register_map(g_register_map);
 	get_memory_addresses(g_memory_addresses);
+	get_arithmetic_imm_reg_map(arithmetic_imm_reg_map);
 	while (offset < buffer.size())
 	{
 		disassemble_8086_opcode(buffer, offset);
