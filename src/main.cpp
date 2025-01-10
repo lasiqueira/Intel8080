ebb59b7877ebad6751b9ab844648b79074f4ce12
lasiqueira <lucas.asiq@gmail.com>
2025-01-10 16:31:15 +0000

    Add function to dump memory

--- src/main.cpp
+++ src/main.cpp
@@ -11,13 +11,13 @@
 //declarations
 ////helpers
 
-uint32_t read_file(const std::string_view filename, std::array<uint8_t, 0xF4240> &buffer);
-
+uint32_t ReadFile(const std::string_view filename, std::array<uint8_t, 0xF4240> &buffer);
+void DumpMemoryToFile(const std::string_view file_name, std::array<uint8_t, 0xF4240>& buffer);
 
 
 //implementations
 
-uint32_t read_file(const std::string_view file_name, std::array<uint8_t, 0xF4240> &buffer)
+uint32_t ReadFile(const std::string_view file_name, std::array<uint8_t, 0xF4240> &buffer)
 {
 	std::ifstream file(file_name.data(), std::ios::in | std::ios::binary);
 	file.unsetf(std::ios::skipws);
@@ -33,13 +33,26 @@ uint32_t read_file(const std::string_view file_name, std::array<uint8_t, 0xF4240
 	return static_cast<uint32_t>(file_size);
 }
 
+void DumpMemoryToFile(const std::string_view file_name, std::array<uint8_t, 0xF4240>& buffer)
+{
+	std::ofstream file(file_name.data(), std::ios::out | std::ios::binary);
+	if (!file)
+	{
+		std::cerr << "Failed to open file for writing: " << file_name << std::endl;
+		return;
+	}
+
+	file.write(reinterpret_cast<const char*>(buffer.data()), buffer.size());
+	file.close();
+}
+
 int main(int argc, char* argv[])
 {
 	std::string file_name = argv[1];
 	
 	//init
 	auto cpu_state = CpuState(); 
-	auto file_size = read_file(file_name, cpu_state.GetMemory());
+	auto file_size = ReadFile(file_name, cpu_state.GetMemory());
 
 	uint8_t stop = 0;
 	while (stop != 1)
@@ -51,5 +64,8 @@ int main(int argc, char* argv[])
 	cpu_state.PrintSegmentRegisters();
 	cpu_state.PrintFlags();
 	
+	// Dump memory to file
+	//DumpMemoryToFile("memory.data", cpu_state.GetMemory());
+
 	return 0;
 }
